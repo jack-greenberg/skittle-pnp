d1f7122acadece59316582004f112cd9dc1f08f9
Zoe McGinnis <zoekauai@gmail.com>
2021-11-17 11:09:04 -0500

    Use most up-to-date image processing code

--- software/image_processing.py
+++ software/image_processing.py
@@ -1,60 +1,44 @@
-from PIL import Image, ImageOps
-import scipy.ndimage as ndimage
+import cv2 as cv
+import numpy as np
 import matplotlib.pyplot as plt
-import cv2
 
-def estimate_color(c, bit, c_error):
-    c_new= c -  c_error
-    if c_new > 127:
-        c_bit= bit
-        c_error= 255 - c_new
-    else:
-        c_bit= 0
-        c_error= -c_new
-    return c_bit, c_error
+def palette(path):
+    # Load the image and convert to grayscale
+    image = cv.imread(path)
+    image = cv.resize(image, (100, 100)) # max: 125 x 125
+    img = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    cv.imwrite('my_img2.jpeg', img)
 
-def image2cga(im):
-    "Produce a sequence of CGA pixels from image im"
-    im_width= im.size[0]
-    for index, (r, g, b) in enumerate(im.getdata()):
-        if index % im_width == 0: # start of a line
-            r_error= g_error= 0
-        r_bit, r_error= estimate_color(r, 1, r_error)
-        g_bit, g_error= estimate_color(g, 2, g_error)
-        yield r_bit|g_bit
+    # Define shade bounds
+    first_lo=np.array([0])
+    first_hi=np.array([63])
+    second_lo = np.array([64])
+    second_hi = np.array([127])
+    third_lo = np.array([128])
+    third_hi = np.array([191])
+    fourth_lo = np.array([192])
+    fourth_hi = np.array([255])
 
-def cvt2cga(imgfn):
-    "Convert an RGB image to (K, R, G, Y) CGA image"
-    inp_im= Image.open(imgfn).convert('L') # assume it's RGB
-    # gray = cv2.cvtColor(inp_im, cv2.COLOR_BGR2GRAY)
-    out_im= Image.new("P", inp_im.size, None)
-    out_im.putpalette( (
-        76, 56, 47,
-        160, 55, 54,
-        240, 113, 15,
-        205, 191, 9
-    ) )
-    out_im.putdata(list(image2cga(inp_im)))
-    return out_im
+    # Mask image by bounds
+    mask1=cv.inRange(img,first_lo,first_hi)
+    mask2=cv.inRange(img,second_lo,second_hi)
+    mask3=cv.inRange(img,third_lo,third_hi)
+    mask4=cv.inRange(img,fourth_lo,fourth_hi)
+
+    # Change image to four colors
+    img[mask1 > 0] = 63 # (76, 56, 47)
+    img[mask2 > 0] = 91 # (160, 55, 54)
+    img[mask3 > 0] = 127 # (240, 113, 15)
+    img[mask4 > 0]= 255 # (205, 191, 9)
+
+    # Write resulting image
+    cv.imwrite("result7.png", img)
+    print(cv.imread("result7.png"))
+    plt.imshow(img)
+    plt.show()
 
 if __name__ == "__main__":
-    import sys, os
-
-    for imgfn in sys.argv[1:]:
-        image = Image.open(imgfn)
-        k = imgfn.rsplit("/",2)[2]
-        new_image = image.resize((125, 125))
-        # new_image = ImageOps.grayscale(new_image)
-        
-        new_image.save("./images/new_"+k)
-        im= cvt2cga("./images/new_"+k)
-        dirname, filename= os.path.split("./images/new_"+k)
-        name, ext= os.path.splitext(filename)
-        newpathname= os.path.join(dirname, "cga-%s.png" % name)
-        #img = ndimage.gaussian_filter(im, sigma=(5, 5, 0), order=0)
-        im.save(newpathname)
-        img = cv2.imread(newpathname)
-        img = ndimage.gaussian_filter(img, sigma=(1, 1, 1), order=0)
-        plt.imshow(img)#, interpolation='nearest')
-        plt.show()
-        #plt.imsave( "./images/test_sample7.png", img)
+    # original_image = "./images/sample7.jpg"
+    original_image = "./images/sample4.jpg"
+    palette(original_image)
+    # print(cv.imread(original_image))
